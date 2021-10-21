d131e615766d2ee5fac4aabd5fd7b1565a745d4f
Илья Скворцов <ilya.skvortsov.2004@mail.ru>
2021-10-21 09:19:21 +0300

    Add DateTime.pas file

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,11 @@
-﻿begin
+﻿function IsLeapYear (year: integer):=  (year mod 4 = 0) or (year mod 400 = 0);
+
+function SecondsInMinute (sec, min: integer):= min * 60;
+
+function LaterInYear(): integer;
+begin
+end;
+
+begin
   
-end
+end.
